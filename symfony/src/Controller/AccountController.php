9dbf97505112915d9a78f2e2c76efc88d7b2fb78
amine <elwadiamine@gmail.com>
2024-11-12 11:42:46 +0100

    symfony - forms part 2

---
+++ symfony/src/Controller/AccountController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Account;
+use App\Form\AccountType;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class AccountController extends AbstractController
+{
+    #[Route('/account', name: 'app_account')]
+    public function index(ManagerRegistry $manager): Response
+    {
+        $data=[];
+        $data['accounts']=$manager->getRepository(Account::class)->findAll();
+        return $this->render('account/index.html.twig', $data);
+    }
+
+    #[Route('/account/create', name: 'app_account_create')]
+    public function create(Request $request,ManagerRegistry $manager): Response
+    {
+        if($request->isMethod('POST')){
+            $account=new Account();
+            $form=$this->createForm(AccountType::class, $account);
+            $form->handleRequest($request);
+            if($form->isSubmitted() && $form->isValid()){
+                $account=$form->getData();
+                $manager->getManager()->persist($account);// 1
+                $manager->getManager()->flush();
+            }
+
+            return $this->redirectToRoute('app_account');
+        }
+
+        $data=[];
+        $data['form']=$this->createForm(AccountType::class)->createView();
+        return $this->render('account/create.html.twig', $data);
+    }
+}
--- symfony/src/Form/AccountType.php
+++ symfony/src/Form/AccountType.php
@@ -2,29 +2,36 @@
 
 namespace App\Form;
 
+use App\Entity\Account;
 use App\Entity\Company;
+use Doctrine\DBAL\Types\TextType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class CompanyType extends AbstractType
+class AccountType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name',TextType::class)
-            ->add('description',TextareaType::class)
+            ->add('firstname')
+            ->add('lastname')
+            ->add('company', EntityType::class, [
+                'class' => Company::class,
+                'choice_label' => 'name',
+            ])
             ->add('submit', SubmitType::class)
         ;
     }
 
+
+
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Company::class,
+            'data_class' => Account::class,
         ]);
     }
 }
