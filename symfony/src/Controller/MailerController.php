15d302b25670eb9ae82c1162bc541ab3899b8bcc
amine <elwadiamine@gmail.com>
2024-12-19 11:51:10 +0100

    symfony email

---
+++ symfony/src/Controller/MailerController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MailerController extends AbstractController
+{
+    #[Route('/email')]
+    public function sendEmail(MailerInterface $mailer): Response
+    {
+        $email = (new Email())
+            ->from('contact@send.saasfactory.dev')
+            ->to('elwadiamine@gmail.com')
+            //->cc('cc@example.com')
+            //->bcc('bcc@example.com')
+            //->replyTo('fabien@example.com')
+            //->priority(Email::PRIORITY_HIGH)
+            ->subject('Time for Symfony Mailer!')
+            ->text('Sending emails is fun again!')
+            ->html('<p>See Twig integration for better HTML integration! <a href="https://symfony.com/doc/current/twig.html"> Link twig</a></p>');
+
+        $mailer->send($email);
+
+        return new Response('Check your email!');
+    }
+}
